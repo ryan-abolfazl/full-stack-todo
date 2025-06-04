f2d748427a4360bbe810bd8ea3ad28fd6e2e5757
Ryan <ryan.abolfazl@gmail.com>
2025-06-05 00:31:39 +0330

    documenting endpoints with spectacular & swagger

--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'todo',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- todo_backend/urls.py
+++ todo_backend/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView # ایمپورت کن
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("todo.urls")),
+    # URLهای مستندسازی
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # شمای OpenAPI (فایل JSON)
+    # این مسیر رو برای رابط کاربری Swagger اضافه کن
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
